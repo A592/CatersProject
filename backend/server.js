b5bf97b94b82f1ea59849f9a7f6ffef29436a58c
Ziyad alawadi <ziadalawadi244@gmail.com>
2024-11-26 20:59:26 +0300

    equipment model

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const Equipment = require('./models/Equipment');
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
